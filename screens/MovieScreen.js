8952b204764751349f78b563257a9d64550ae8b8
fabianmuema <muemafabian@gmail.com>
2020-03-08 13:49:52 +0300

    shows screen

--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -1,27 +1,150 @@
 import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, Platform, StyleSheet, Text, TouchableOpacity, View, TextComponent } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-import * as WebBrowser from 'expo-web-browser';
+import { SearchBar } from 'react-native-elements';
 
-import { MonoText } from '../components/StyledText';
 
-export default function MusicScreen() {
+export default function MovieScreen() {
     return (
         <View style={styles.container}>
             <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
                 <View style={styles.helpContainer}>
-                    <Text style={styles.textAlign}>Hello, Music Screen</Text>
+                    <SearchBar placeholder = 'Search movies/shows/music...'
+                    style = {
+                        styles.searchBar
+                    }
+                    platform = {
+                        'ios'
+                    }
+                    round = {
+                        true
+                    }
+                    containerStyle = {
+                        {
+                        backgroundColor: 'transparent',
+                        width: 290,
+                        borderWidth: 0,
+
+                        }
+                    }
+                    inputContainerStyle = {
+                        {
+                        borderRadius: 20
+                        }
+                    }
+                    cancelButtonTitle = {true}
+                    inputStyle = {
+                        {
+                        backgroundColor: 'lightgrey',
+                        fontSize: 14,
+                        paddingLeft: 5
+                        }
+                    }
+                    lightTheme = {
+                        true
+                    }
+                    clearIcon={true}
+                    />
+                </View>
+                <View style={styles.featuredMovies}>
+                    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                        <Image source={{uri: 'https://fanart.tv/detailpreview/fanart/tv/364093/tvthumb/star-trek-picard-5e2bcbcd25756.jpg'}} style={styles.featuredMovieImages}/>
+                        <Image source={{ uri: 'https://fanart.tv/detailpreview/fanart/tv/295760/tvthumb/dcs-legends-of-tomorrow-58a073b3c0df8.jpg'}} style={styles.featuredMovieImages}/>
+                        <Image source={{ uri: 'https://fanart.tv/detailpreview/fanart/tv/372081/tvthumb/hunters-5e14697b2dd38.jpg' }} style={styles.featuredMovieImages} />
+                        <Image source={{ uri: 'https://fanart.tv/detailpreview/fanart/tv/277366/tvthumb/jim-hensons-creature-shop-challenge-5e5a2f5bafe28.jpg' }} style={styles.featuredMovieImages} />
+                        <Image source={{ uri: 'https://fanart.tv/detailpreview/fanart/tv/332331/tvthumb/altered-carbon-5a7446a30d1e7.jpg' }} style={styles.featuredMovieImages} />
+                        <Image source={{ uri: 'https://fanart.tv/detailpreview/fanart/tv/354265/tvthumb/deputy-5e4ea71403932.jpg' }} style={styles.featuredMovieImages} />
+                        <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/322/988/posters/thumb/b2fa5f3d6c.jpg.webp' }} style={styles.featuredMovieImages} />
+                    </ScrollView>
+                </View>
+
+                <View style={styles.recentMovies}>
+                    <View style={{flex: 1, flexDirection: "row"}}>
+                       <Text style={styles.BoldText}>Recommended</Text><Text style={styles.greyText}> Movies</Text>
+                    </View>
+                    <View>
+                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                            <Image source={{uri: 'https://walter.trakt.tv/images/movies/000/307/054/posters/thumb/ab1a43b360.jpg.webp'}} style={styles.nowPlayingMovieImages}/>
+                            <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/360/095/posters/thumb/25b1652585.jpg.webp'}} style={styles.nowPlayingMovieImages}/>
+                            <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/211/394/posters/thumb/3d953c303e.jpg.webp' }} style={styles.nowPlayingMovieImages} />
+                            <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/277/572/posters/thumb/770740180b.jpg.webp' }} style={styles.nowPlayingMovieImages} />
+                            <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/306/046/posters/thumb/660bc9e167.jpg.webp' }} style={styles.nowPlayingMovieImages} />
+                            <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/301/015/posters/thumb/77cebb5f45.jpg.webp' }} style={styles.nowPlayingMovieImages} />
+                            <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/322/988/posters/thumb/b2fa5f3d6c.jpg.webp' }} style={styles.nowPlayingMovieImages} />
+                        </ScrollView>
+                    </View>
+                </View>
+
+                <View style={styles.recentMovies}>
+                    <View style={{flex: 1, flexDirection: "row"}}>
+                       <Text style={styles.BoldText}>Recent</Text><Text style={styles.greyText}> Movies</Text>
+                    </View>
+                    <View>
+                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                            <Image source={{uri: 'https://walter.trakt.tv/images/movies/000/307/054/posters/thumb/ab1a43b360.jpg.webp'}} style={styles.nowPlayingMovieImages}/>
+                            <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/360/095/posters/thumb/25b1652585.jpg.webp'}} style={styles.nowPlayingMovieImages}/>
+                            <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/211/394/posters/thumb/3d953c303e.jpg.webp' }} style={styles.nowPlayingMovieImages} />
+                            <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/277/572/posters/thumb/770740180b.jpg.webp' }} style={styles.nowPlayingMovieImages} />
+                            <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/306/046/posters/thumb/660bc9e167.jpg.webp' }} style={styles.nowPlayingMovieImages} />
+                            <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/301/015/posters/thumb/77cebb5f45.jpg.webp' }} style={styles.nowPlayingMovieImages} />
+                            <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/322/988/posters/thumb/b2fa5f3d6c.jpg.webp' }} style={styles.nowPlayingMovieImages} />
+                        </ScrollView>
+                    </View>
+                </View>
+                <View style={styles.recentMovies}>
+                    <View style={{flex: 1, flexDirection: "row"}}>
+                       <Text style={styles.BoldText}>Popular</Text><Text style={styles.greyText}> Movies</Text>
+                    </View>
+                    <View>
+                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                            <Image source={{uri: 'https://walter.trakt.tv/images/movies/000/307/054/posters/thumb/ab1a43b360.jpg.webp'}} style={styles.nowPlayingMovieImages}/>
+                            <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/360/095/posters/thumb/25b1652585.jpg.webp'}} style={styles.nowPlayingMovieImages}/>
+                            <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/211/394/posters/thumb/3d953c303e.jpg.webp' }} style={styles.nowPlayingMovieImages} />
+                            <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/277/572/posters/thumb/770740180b.jpg.webp' }} style={styles.nowPlayingMovieImages} />
+                            <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/306/046/posters/thumb/660bc9e167.jpg.webp' }} style={styles.nowPlayingMovieImages} />
+                            <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/301/015/posters/thumb/77cebb5f45.jpg.webp' }} style={styles.nowPlayingMovieImages} />
+                            <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/322/988/posters/thumb/b2fa5f3d6c.jpg.webp' }} style={styles.nowPlayingMovieImages} />
+                        </ScrollView>
+                    </View>
                 </View>
             </ScrollView>
         </View>
     );
 }
 
-MusicScreen.navigationOptions = {
+MovieScreen.navigationOptions = {
     header: null,
 };
 
 const styles = StyleSheet.create({
+    nowPlayingMovieImages: {
+        width: 80,
+        height: 120,
+        marginRight: 7,
+        borderRadius: 10
+    },
+    BoldText: {
+        color: "black",
+        fontWeight: "bold",
+        paddingBottom: 8
+    },
+    greyText: {
+        color: 'grey',
+    },
+    recentMovies: {
+        marginLeft: 5,
+        marginBottom: 7
+    },
+    featuredMovies: {
+        marginLeft: 5,
+        marginBottom: 7
+    },
+    featuredMovieImages: {
+        width: 200,
+        height: 100,
+        marginRight: 10,
+        borderRadius: 10
+    },
     container: {
         flex: 1,
         backgroundColor: '#fff',
