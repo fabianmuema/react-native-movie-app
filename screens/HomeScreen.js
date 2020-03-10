de69a4441bfb78ca1d52615bfe7840ea8c433cf5
fabianmuema <muemafabian@gmail.com>
2020-03-10 11:35:56 +0300

    creating the modal

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, Modal, TouchableOpacity } from 'react-native';
-import { ScrollView} from 'react-native-gesture-handler';
+import React, { Component, useState} from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 import { LinearGradient } from 'expo-linear-gradient';
 import { SearchBar } from 'react-native-elements';
+import Modal from 'react-native-modal';
 
 const GreeterText = [
   'watch today?', 'do today?', 'stream today?', 'listen to today?', 'enjoy today?'
@@ -10,12 +11,30 @@ const GreeterText = [
 
 var randomNumber = Math.floor(Math.random() * (GreeterText.length))
 
-export default function MovieScreen() {
-  const [modalVisible, setModalVisible] = useState(false);
+export default class MovieScreen extends Component {
+  state = {
+    isModalVisible: false
+  };
+
+  toggleModal = () => {
+    this.setState({ isModalVisible: !this.state.isModalVisible });
+  };
+
+  render() {
+
   return (
+
     <View style={styles.container}>
       <ScrollView>
 
+        <Modal isVisible={this.state.isModalVisible} animationIn="slideInUp" animationInTiming={300} hasBackdrop={true} backdropColor="white" onBackdropPress={() => this.setState({ isVisible: false })}   onSwipeComplete={() => this.setState({ isVisible: false })}
+   swipeDirection={['up','down']} backdropOpacity={1}
+   >
+          <View style={{ flex: 1 }}>
+            <Text>Hello!</Text>
+            <Button title="Hide modal" onPress={this.toggleModal} />
+          </View>
+        </Modal>
         <LinearGradient colors={['#000000', '#000']} style={styles.linearGradient}></LinearGradient>
 
         <View style={styles.Greeter}>
@@ -66,55 +85,17 @@ export default function MovieScreen() {
 
         <View style={styles.Movies}>
         <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
+          <TouchableOpacity onPress={this.toggleModal}>
             <Image source={{uri: 'https://fanart.tv/detailpreview/fanart/tv/364093/tvthumb/star-trek-picard-5e2bcbcd25756.jpg'}} style={styles.movieImages}/>
           </TouchableOpacity>
-          < TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          } >
+          <TouchableOpacity onPress={this.toggleModal}>
           <Image source={{ uri: 'https://fanart.tv/detailpreview/fanart/tv/295760/tvthumb/dcs-legends-of-tomorrow-58a073b3c0df8.jpg'}} style={styles.movieImages}/>
-          </ TouchableOpacity>
-          < TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          } >
+          </TouchableOpacity>
           <Image source={{ uri: 'https://fanart.tv/detailpreview/fanart/tv/372081/tvthumb/hunters-5e14697b2dd38.jpg' }} style={styles.movieImages} />
-          </ TouchableOpacity>
-          < TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          } >
           <Image source={{ uri: 'https://fanart.tv/detailpreview/fanart/tv/277366/tvthumb/jim-hensons-creature-shop-challenge-5e5a2f5bafe28.jpg' }} style={styles.movieImages} />
-          </ TouchableOpacity>
-          < TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          } >
           <Image source={{ uri: 'https://fanart.tv/detailpreview/fanart/tv/332331/tvthumb/altered-carbon-5a7446a30d1e7.jpg' }} style={styles.movieImages} />
-          </ TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://fanart.tv/detailpreview/fanart/tv/354265/tvthumb/deputy-5e4ea71403932.jpg' }} style={styles.movieImages} />
-          </ TouchableOpacity>
-          < TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          } >
           <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/322/988/posters/thumb/b2fa5f3d6c.jpg.webp' }} style={styles.movieImages} />
-          </ TouchableOpacity>
 
         </ScrollView>
       </View>
@@ -122,55 +103,13 @@ export default function MovieScreen() {
        <View style={styles.nowPlayingMovies}>
         <Text style={styles.popularText}>Popular Movies</Text>
         <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-        <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{uri: 'https://walter.trakt.tv/images/movies/000/307/054/posters/thumb/ab1a43b360.jpg.webp'}} style={styles.nowPlayingMovieImages}/>
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/360/095/posters/thumb/25b1652585.jpg.webp'}} style={styles.nowPlayingMovieImages}/>
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/211/394/posters/thumb/3d953c303e.jpg.webp' }} style={styles.nowPlayingMovieImages} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/277/572/posters/thumb/770740180b.jpg.webp' }} style={styles.nowPlayingMovieImages} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/306/046/posters/thumb/660bc9e167.jpg.webp' }} style={styles.nowPlayingMovieImages} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/301/015/posters/thumb/77cebb5f45.jpg.webp' }} style={styles.nowPlayingMovieImages} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/322/988/posters/thumb/b2fa5f3d6c.jpg.webp' }} style={styles.nowPlayingMovieImages} />
-          </TouchableOpacity>
 
         </ScrollView>
       </View>
@@ -178,55 +117,13 @@ export default function MovieScreen() {
       <View style={styles.Albums}>
         <Text style={styles.likedSeriesText}>Latest Albums</Text>
         <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-        <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://charts-static.billboard.com/img/2019/12/roddy-ricch-2vu-please-excuse-me-for-being-antisocial-dee-310x310.jpg' }} style={styles.albumImages} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://charts-static.billboard.com/img/2019/09/taylor-swift-70p-lover-a0i-310x310.jpg' }} style={styles.albumImages} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://charts-static.billboard.com/img/2019/11/soundtrack-000-frozen-ii-yhp-310x310.jpg' }} style={styles.albumImages} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://charts-static.billboard.com/img/2019/10/summer-walker-1sj-over-it-dlk-310x310.jpg' }} style={styles.albumImages} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://charts-static.billboard.com/img/2017/10/billie-eilish-1e3-dont-smile-at-me-hbg-310x310.jpg' }} style={styles.albumImages} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://charts-static.billboard.com/img/2019/12/camila-cabello-p0o-310x310.jpg' }} style={styles.albumImages} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://charts-static.billboard.com/img/2018/06/juice-wrld-f0a-310x310.jpg' }} style={styles.albumImages} />
-          </TouchableOpacity>
 
         </ScrollView>
       </View>
@@ -235,55 +132,13 @@ export default function MovieScreen() {
       <View style={styles.Series}>
         <Text style={styles.likedSeriesText}>Trending Artists</Text>
         <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-        <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://charts-static.billboard.com/img/2007/12/maroon-5-9st.jpg?1' }} style={styles.artistImages} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://charts-static.billboard.com/img/2010/12/nicki-minaj-thi.jpg?1' }} style={styles.artistImages} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://charts-static.billboard.com/img/2005/12/eminem-1xq.jpg?1' }} style={styles.artistImages} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://charts-static.billboard.com/img/2012/11/kendrick-lamar-h1y.jpg?1' }} style={styles.artistImages} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://charts-static.billboard.com/img/2006/12/beyonce-000-artist-chart-cci.jpg?1' }} style={styles.artistImages} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://charts-static.billboard.com/img/2007/12/luke-bryan-kl4.jpg?1' }} style={styles.artistImages} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://charts-static.billboard.com/img/2006/12/chris-brown-cte.jpg?1' }} style={styles.artistImages} />
-          </TouchableOpacity>
 
         </ScrollView>
       </View>
@@ -291,84 +146,22 @@ export default function MovieScreen() {
       <View style={styles.nowPlayingMovies}>
         <Text style={styles.popularText}>Popular Tv Shows</Text>
         <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-        <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{uri: 'https://walter.trakt.tv/images/movies/000/307/054/posters/thumb/ab1a43b360.jpg.webp'}} style={styles.nowPlayingMovieImages}/>
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/360/095/posters/thumb/25b1652585.jpg.webp'}} style={styles.nowPlayingMovieImages}/>
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/211/394/posters/thumb/3d953c303e.jpg.webp' }} style={styles.nowPlayingMovieImages} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/277/572/posters/thumb/770740180b.jpg.webp' }} style={styles.nowPlayingMovieImages} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/306/046/posters/thumb/660bc9e167.jpg.webp' }} style={styles.nowPlayingMovieImages} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/301/015/posters/thumb/77cebb5f45.jpg.webp' }} style={styles.nowPlayingMovieImages} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress = {
-            () => {
-              setModalVisible(true);
-            }
-          }>
           <Image source={{ uri: 'https://walter.trakt.tv/images/movies/000/322/988/posters/thumb/b2fa5f3d6c.jpg.webp' }} style={styles.nowPlayingMovieImages} />
-          </TouchableOpacity>
 
         </ScrollView>
       </View>
-      <Modal
-        animationType="slide"
-        transparent={false}
-        presentationStyle="formSheet"
-        visible={modalVisible}>
-        <View style={{ marginTop: 22 }}>
-          <View>
-            <Text>Hello World!</Text>
-
-              <TouchableOpacity
-                onPress = {
-                  () => {
-                    setModalVisible(!modalVisible);
-                  }
-                }
-                style={{ backgroundColor: 'blue' }}>
-                <Text style={{ fontSize: 20, color: '#fff' }}>Close Modal</Text>
-              </TouchableOpacity>
-          </View>
-        </View>
-      </Modal>
 
       </View>
       </ScrollView>
     </View>
   );
+        }
 }
 
 MovieScreen.navigationOptions = {
